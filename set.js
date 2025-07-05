0e325e430ea1dcfae03d85c5412ec6bd2637e255
mateusrochadevlp <mateusbatista0204@gmail.com>
2025-07-05 01:10:54 -0300

    Corrige método union no set.js

--- set.js
+++ set.js
@@ -1,24 +1,42 @@
-module.exports = class Univ {
-    constructor() {
-        this.itens = {}
+module.exports = class Custom {
+  constructor() {
+    this.itens = {};
+  }
+//check that the element or value that the user has submitted is in the object
+  has(Element) {
+    return this.itens.hasOwnProperty(Element);
+  }
+  add(Element) {
+    if (!this.has(Element)) {
+      this.itens[Element] = Element;
+      return true;
     }
-    has(element) {
-        return this.itens.hasOwnProperty(element);
+  }
+  remove(Element) {
+    if (this.has(Element)) {
+      delete this.itens[Element];
+      return true;
     }
-    add(element) {
-        if (!this.iens.hasOwnProperty(element)) {
-            return this.itens[element] = element
-        }
-        return false
+    return false;
+  }
+  //clean/ "reset" object 
+  clear () {
+    this.itens = {};
+  }
+  //return values of object
+  values(){
+    return Object.values(this.itens);
+  }
+  union(otherSet) {
+    const unionSet = new Custom();
+    let values = this.values();
+    for (let i = 0; i < values.length; i++){
+      unionSet.add(values[i]);
     }
-    delete(element) {
-        if (this.has(element)) {
-            delete this.itens[element];
-            return true;
-        }
-        return false
+    values = otherSet.values();
+    for (let i = 0; i < values.length; i++) {
+      unionSet.add(values[i]);
     }
-    clear() {
-        this.itens = {}
-    }
-}
+    return unionSet;
+  }
+};
