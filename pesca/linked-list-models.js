5916854ab27fd4fc834555164bf21a184d59d095
Mateus Batista <mateusbatista0204@gmail.com>
2025-11-08 19:22:33 -0300

    Delete

--- pesca/linked-list-models.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Node {
-    constructor(element) {
-        this.element = element;
-        this.next = null;
-    }
-}
-
-const defaultEquals  = (a, b) => a === b;
-console.log(defaultEquals(1, 2))
